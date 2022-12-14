abc9e8c6ea74393a31223abf5293ace92446bce3
Madidolm <120350641+Madidolm@users.noreply.github.com>
2022-12-14 20:53:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ from time import sleep
 from pygame import display, event, image
 from time import sleep
 from alash import alash
+def find_index_from_xy(x, y):
+    row = y // gc.IMAGE_SIZE
+    col = x // gc.IMAGE_SIZE
+    index = row * gc.NUM_TILES_SIDE + col
+    return row, col, index
