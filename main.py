81ab4f29614801ce0198f4f94c9965cf7ae7216e
Madidolm <120350641+Madidolm@users.noreply.github.com>
2022-12-14 20:53:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import game_config as gc
 
 from pygame import display, event, image
 from time import sleep
+from pygame import display, event, image
+from time import sleep
+from alash import alash
